6a5add4e0186e49c53b4222f9eb0e90c5776d1ab
samir-mohamed <71074590+samir-mohamed@users.noreply.github.com>
2024-07-21 23:54:38 +0300

    [217] [Time Beats: 98.52%] [Memory Beats: 13.60%] - LeetPush

---
+++ 217-Contains-Duplicate.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        var dict = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.Contains(nums[i])) return true;
+            else dict.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
